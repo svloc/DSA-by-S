b7e5fd6a0cd2c7f68a3d2c127fc96af7248a07cd
svloc <svloc39@gmail.com>
2022-06-19 20:34:25 +0530

    Reverse Array

---
+++ Reverse.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Reverse(int arr[],int n){
+    for(int i=n-1;i>=0;i--)
+    cout<<arr[i]<<" ";
+}
+
+int main(){
+ int n;
+    cout<<"Enter N: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter N Element: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    Reverse(arr,n);
+    return 0;   
+}
